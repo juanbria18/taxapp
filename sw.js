0c031b44e1fdbe825507f1f9389dc71ecaf26fb2
juanbria18 <juanbria18@gmail.com>
2026-04-24 15:23:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'tax-usa-v2';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
